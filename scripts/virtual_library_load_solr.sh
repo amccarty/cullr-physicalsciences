3a84c1ad7ce36533b8a199ec72e11cece803df8c
jgr25 <jgr25@09ede299-dab1-0410-a1a2-91d2876a6cab>
2012-06-12 18:41:50 +0000

    use shopt -s nullglob to avoid errors from loading empty files into solr

--- scripts/virtual_library_load_solr.sh
+++ scripts/virtual_library_load_solr.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-pushd /cullr/instances/physicalsciences/solr_add/
+pushd /cullr/instances/physicalsciences/solr_add/bibids/
 
-for bib in bibids/*.xml
+shopt -s nullglob
+for bib in *.xml
 do
 /cullr/instances/physicalsciences/cullr_physicalsciences/scripts/physicalsciences_postx.sh "$bib"
 done
