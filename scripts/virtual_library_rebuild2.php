7a8918b993df67c249659125da547141311c4cd1
jgr25 <jgr25@09ede299-dab1-0410-a1a2-91d2876a6cab>
2012-08-15 17:16:57 +0000

    testing new rebuild_solr_index()

---
+++ scripts/virtual_library_rebuild2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * virtual_library_rebuild.php - recreate psl virtual library
+ * (run from the command line)
+ */
+include_once "VL_PhysicalSciences.php";
+
+$vl = new VL_PhysicalSciences();
+
+// look for the -production argument to rebuild the production version
+//if (isset($argv[1]) && ($argv[1] == 'production')) {
+//	$vl->use_production_plop(TRUE);
+//	}
+	
+// find every bibid in the virtual library
+$vl->delete_biblist();
+$vl->make_biblist();
+
+// rebuild the index solr instance
+$vl->rebuild_solr_index();
+
+?>
