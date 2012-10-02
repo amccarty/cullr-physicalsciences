259126d51587410442d76b9b611f43dac05f062e
James Reidy <jgr25@cornell.edu>
2012-10-02 18:43:30 +0000

    allow checking for the production server argument

--- scripts/virtual_library_rebuild2.php
+++ scripts/virtual_library_rebuild2.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * virtual_library_rebuild.php - recreate psl virtual library
+ * virtual_library_rebuild2.php - recreate psl virtual library
  * (run from the command line)
  */
 include_once "VL_PhysicalSciences.php";
@@ -8,9 +8,9 @@ include_once "VL_PhysicalSciences.php";
 $vl = new VL_PhysicalSciences();
 
 // look for the -production argument to rebuild the production version
-//if (isset($argv[1]) && ($argv[1] == 'production')) {
-//	$vl->use_production_plop(TRUE);
-//	}
+if (isset($argv[1]) && ($argv[1] == 'production')) {
+	$vl->use_production_plop(TRUE);
+	}
 	
 // find every bibid in the virtual library
 $vl->delete_biblist();
