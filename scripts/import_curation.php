2fea245b36c0c4fb290554b0b50768d78d613820
jgr25 <jgr25@09ede299-dab1-0410-a1a2-91d2876a6cab>
2012-11-13 21:27:08 +0000

    start on load_curation

---
+++ scripts/import_curation.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * import_curation.php - load curated records into a virtual library
+ * (run from the command line)
+ * php import_curation.php [ test | production ] filename.xml
+ */
+include_once "VL_PhysicalSciences.php";
+
+iconv_set_encoding("input_encoding", "UTF-8");
+iconv_set_encoding("output_encoding", "UTF-8");
+iconv_set_encoding("internal_encoding", "UTF-8");
+
+$vl = new VL_PhysicalSciences();
+
+// look for the -production argument to rebuild the production version
+$version = 'test';
+if (isset($argv[1]) && ($argv[1] == 'production')) {
+	$vl->use_production_plop(TRUE);
+	$version = 'production';
+	}
+if (isset($argv[2])) {
+	$input_filename = $argv[2];
+	if (!is_file($input_filename)) {
+		die("Need a better filename than '$input_filename'." . PHP_EOL);
+		}
+	}
+else {
+	die("Usage: php import_curation.php [ test | production ] filename.xml" . PHP_EOL);
+	}
+
+$curation = $vl->load_curation($input_filename);
+var_dump($curation);
+?>
